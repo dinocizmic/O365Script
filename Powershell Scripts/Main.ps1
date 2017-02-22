9b02f80d2384a4b7fcb0c8d377f422eba3df26c0
dcizmic <dino.cizmic@blucora.com>
2017-02-22 08:36:30 -0800

    Added changes to Main
    Will now connect to O365, add new user using MSol

--- Powershell Scripts/Main.ps1
+++ Powershell Scripts/Main.ps1
@@ -1,25 +1,40 @@
-﻿$Confirmation = Read-Host "Are you sure you want to proceed: "
+﻿$Confirmation = Read-Host "Do you want to connect to O365?  "
 if ($Confirmation -eq 'y')
 { 
-{
-    echo 'hello my name is yellow'
+ 
     
-}
-$confirmation = Read-Host "Do you want to create a new user or put a pst on hold? [New/Hold]"
-if ($Confirmation -eq 'new')
-{
-$DisplayName = Read-Host -Prompt 'Input the DisplayName'
-$FirstName = Read-Host -Prompt 'Input the FirstName'
-$LastName = Read-Host -Prompt 'Input the LastName'
-$UserPrincipalName = Read-Host 'Input the Email'
-$Password = Read-Host -Prompt 'Input password'
+    $UserCredential = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/  -Credential $UserCredential -Authentication Basic -AllowRedirection
+    Connect-MsolService -Credential $UserCredential
+    
+ 
+
+$confirmation = Read-Host "Do you want to create a new user or remove a user? [New/Remove]"
+    if ($Confirmation -eq 'new')
+  {
+    $DisplayName = Read-Host -Prompt 'Input the DisplayName'
+    $FirstName = Read-Host -Prompt 'Input the FirstName'
+    $LastName = Read-Host -Prompt 'Input the LastName'
+    $UserPrincipalName = Read-Host 'Input Firstname.LastName@Blucora.com'
 
 
-#New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UserPrincipalName -UsageLocation US -LicenseAssignment Blucora:ENTERPRISEE3 [-Password $password]
-Write-Host "The DisplayName is '$DisplayName' The first name is '$Firstname' The LastName is '$LastName' The email will be '$UserPrincipleName' and the password is '$password'"
-}
-if ($Confirmation -eq 'hold')
-{
-echo 'I will hold'
-}
+
+     Write-Host "The DisplayName is $DisplayName The first name is $Firstname The LastName is $LastName The email will be $UserPrincipalName and the password is $password"
+
+    $Confirmation = Read-Host "Should I create a new account with the provided Credentials? [Y/N]"
+        if ($Confirmation -eq 'y')
+            {
+                echo 'New account will be created...'
+                New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UserPrincipalName -UsageLocation US -LicenseAssignment Blucora:ENTERPRISEPACK
+            }
+        if ($Confirmation -eq 'n')
+            {
+                echo 'New account will not be created, Disconnected from Office 365'
+                Remove-PSSession $Session
+            }
+  }
+    if ($Confirmation -eq 'Remove')
+ {
+    echo 'I will hold'
+ }
 }
--- Powershell Scripts/TestAddUser.ps1
+++ Powershell Scripts/TestAddUser.ps1
@@ -1,8 +1,9 @@
 ﻿$DisplayName = Read-Host -Prompt 'Input the DisplayName'
 $FirstName = Read-Host -Prompt 'Input the FirstName'
 $LastName = Read-Host -Prompt 'Input the LastName'
-$UserPrincipalName = Read-Host $FirstName'.'$lastname'@blucora.com'
+$UserPrincipalName = Read-Host 'Input firstname.lastname'
 $Password = Read-Host -Prompt 'Input password'
 
 
-New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UserPrincipalName -UsageLocation US -LicenseAssignment Blucora:ENTERPRISEE3 [-Password $password]
+#New-MsolUser -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UserPrincipalName -UsageLocation US -LicenseAssignment Blucora:ENTERPRISEE3 [-Password $password]
+Write-Host "The DisplayName is '$DisplayName' The first name is '$Firstname' The LastName is '$LastName' The email will be $UserPrincipalName@Blucora.com and the password is '$password'"
